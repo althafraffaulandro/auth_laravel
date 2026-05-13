86e31f537db41c6e7a0ea73f4ff81e16992f33ab
althafraffaulandro <althafraffaulandro008@gmail.com>
2026-05-13 10:12:44 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route Group Middleware Auth untuk bagian backend Admin
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+});
